1729120f738fbe9a7e4dddead18a36e8e08c4ae9
Leonel <leonelombardo1@gmail.com>
2023-01-25 17:10:57 -0300

    updating dependencies and tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,33 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sans": ["Inter Tight", "Inter", "sans-serif", "system-ui"]
+      },
+      colors: {
+        primary: {
+          500: "#1a1a1a"
+        },
+        secondary: {
+          500: "#f9f9f9"
+        },
+        success:{
+          500: "#39b85b"
+        },
+        error:{
+          500: "#d92e2e"
+        },
+        lightblue: {
+          500: "#06f"
+        }
+      },
+      boxShadow: {
+        custom: "10px 10px 20px 0px #efefef"
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp')
+  ],
 }
