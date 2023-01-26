1402af95e57e683b11178964d01cab96cfedd4b8
Leonel <leonelombardo1@gmail.com>
2023-01-26 16:09:48 -0300

    context: creating global context / utils: toast.js for notifications / app: context and toaster component for showing toasts up implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./index.css"
+import { Toaster } from "react-hot-toast"
+
+import { Context } from "./context/context"
 
 import { Home } from "./views/Home"
 
 export const App = () => {
+  const [ cart, setCart ] = useState([])
+  
+  const context = { cart, setCart }
+
   return (
     <>
+    <Toaster/>
+    <Context.Provider value={context}>
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
         </Routes>
       </Router>
+    </Context.Provider>
     </>
   )
 }
